6ea5e4149e6ce6d8063e0c8ee14c5605fd6895f8
Sankowski <thomas-sankowski@t-online.de>
2017-11-30 14:18:40 +0100

    Add:CharacterMovement UPDATE: GitIgnore to Igrnore .vs files

---
+++ Walking Dead/ZombieHigh/Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private int speed = 10;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
